c934b7f1c761e30ed8524133a26b76c0a04905a9
Colin Johnson <colin@colinjohnson.net>
2020-11-24 17:44:24 -0800

    Rewrite with pandas

--- messageAnalysis.py
+++ messageAnalysis.py
@@ -1,143 +1,93 @@
-"""
-Author: Colin Johnson
 
-This program is functional as of September 2018, future changes to the facebook data
-format may break it horribly
+# Standard library imports
+import glob
+import json
+import argparse
 
-"""
+# Third-party imports
+import pandas as pd
 
-import glob, os, json, csv, collections
-from pprint import pprint
-from datetime import datetime
+# Get command line parameters
+parser = argparse.ArgumentParser(description='''
+    Process facebook json message data. The messages directory from the data download must 
+    be in the current working directory. 
+''')
+parser.add_argument(
+    "--name", 
+    help="Name of the owner facebook, as it appear in messages download.",
+    required=True
+)
+parser.add_argument(
+    "--friends", 
+    nargs="+", 
+    help="Name of friends to include in stats.",
+    required=True
+)
+args = parser.parse_args()
+my_name = args.name
+friends = args.friends
 
-def readJSON(includeGroups=False):
+# iterate over every message file, grabbing data from each
+rows = []
+for filePath in glob.glob("messages/inbox/**/message_*.json"):
 
-	# dictionary to store message data
-	data = []
+    # read the message file into a dictionary
+    message_file_json = ""
+    with open(filePath) as file:
+        message_file_json = file.read()
+    message_file = json.loads(message_file_json)
 
-	# find appropriate data files
-	for filePath in glob.glob("inbox/*/message.json"):
-		
-		# read all the message.json files
-		with open(filePath, 'r') as jsonFile:
+    # get message file metadata
+    participants = message_file["participants"]
+    messages = message_file["messages"]
 
-			# store data in a new dictionary
-			try:
-				jsonData = json.load(jsonFile)
+    # discard group chats
+    if len(participants) != 2:
+        continue
 
-				# avoid adding group chats
-				if includeGroups or len(jsonData["participants"]) == 2:
+    # get the name of the friend the current message file is for
+    friend_name = participants[0]["name"]
 
-					data.append({
-						"title": jsonData["title"], 
-						"messages": jsonData["messages"], 
-						"count": len(jsonData["messages"]),
-						"participants": jsonData["participants"]})
+    # create a row for each message
+    for message in messages:
 
-			except Exception as e:
-				#print("error parsing ", filePath)
-				pass
+        # ignore non-text messages e.g. pictures, shares, calls etc.
+        msgtype = message.get('type')
+        if msgtype != 'Generic':
+            continue
 
-	return data;
+        sent_by_friend = message.get("sender_name") != my_name
+        timestamp = message.get("timestamp_ms")
+        content = message.get("content")
+        length = len(content) if content else 0
 
-def parseData(data, extent, printTotals=False, characters=False):
+        rows.append({
+            'friend': friend_name,
+            'sent_by_friend': sent_by_friend,
+            'length': length,
+            'timestamp': timestamp
+        })
 
-	# sort and trim data
-	data = sorted(data, key=lambda k: k["count"], reverse=True)[:extent]
 
-	# print total message counts
-	for convo in data:
-		print(convo["count"], " messages from ",  convo["title"]);
+df = pd.DataFrame(rows)
+df['timestamp'] = pd.to_datetime(df['timestamp'], unit="ms")
+df['friend'] = pd.Categorical(df.friend)
 
-	# make a template dictionary of dates between the earlist and most recent messages
-	earliestMS = 10e20
-	latestMS = 0
-	dates = {}
+for x in df['friend'].unique():
+    print(x)
+df = df[df['friend'].isin(friends)]
 
-	for convo in data:
-		late = convo["messages"][0]["timestamp_ms"]
-		early = convo["messages"][len(convo["messages"]) - 1]["timestamp_ms"]
-		latestMS = late if late > latestMS else latestMS
-		earliestMS = early if early < earliestMS else earliestMS
+date_index = df.set_index('timestamp')
+month_sums = date_index.groupby([pd.Grouper(freq="M"), 'friend']).count().dropna()
+del month_sums['sent_by_friend']
+month_sums = month_sums.reset_index().pivot(index='timestamp', columns='friend').fillna(0)
 
-	earlistStr = datetime.fromtimestamp(earliestMS / 1000).strftime("%B %d %Y")
-	latestStr = datetime.fromtimestamp(latestMS / 1000).strftime("%B %d %Y")
-	print("from {} to {}.".format(earlistStr, latestStr.strip()))
+print(month_sums.to_string())
 
-	for i in range(earliestMS, latestMS, int((latestMS - earliestMS) / 12000)):
-		dates[datetime.fromtimestamp(i / 1000).strftime("%B %Y")] = {}
+ax = month_sums.plot.area(figsize=(10,5), linewidth=0)
+ax.get_figure().savefig('stacked.png', dpi=300)
 
-	# add monthly counts for each conversation
-	for convo in data:
+lines = month_sums.plot.line(figsize=(10,5))
+lines.get_figure().savefig('lines.png', dpi=300)
 
-		# make a deep copy of the template dates dictionary to avoid referencing issues
-		datesCopy = dict(dates)
-		for key in datesCopy.keys():
-			datesCopy[key] = {"sent": 0, "received": 0}
-		convo["countsMonthly"] = collections.OrderedDict(datesCopy)
-
-		# count messages/characters by month
-		for message in convo["messages"]:
-			date = datetime.fromtimestamp(message["timestamp_ms"] / 1000)
-			dateStr = date.strftime("%B %Y")
-
-			# increment the count by 1 or by the number of characters in "content"
-			content = message.get("content")
-			increment = len(content) if (characters and content) else 1
-
-			# track sent and recieved messages separately (you aren't in the participants field)
-			if message["sender_name"] in convo["participants"][0]["name"]: # THIS IS BUGGY, ASSUMES SENDER NAME FIRST, BAD FOR GROUP CHATS
-				convo["countsMonthly"][dateStr]["received"] += increment
-			else:
-				convo["countsMonthly"][dateStr]["sent"] += increment
-
-	for i in range(len(data)):
-		del data[i]["messages"]
-
-	return data
-
-"""Writes data to fileName.csv in the local directory. If separate is set to true, send messages
-and received messages will be counted in separate columns"""
-def writeData(data, fileName, separate=False):
-
-	# write data to csv file
-	with open(fileName, "w", newline='') as outFile:
-		writer = csv.writer(outFile)
-
-		# write header row with titles
-		titles = [""]
-		for convo in data:
-			if separate:
-				titles.append(convo["title"] + " - sent")
-				titles.append(convo["title"] + " - received")
-			else:
-				titles.append(convo["title"]) 
-		writer.writerow(titles)
-
-		# write data rows
-		for date in data[0]["countsMonthly"]:
-			rowData = []
-			for convo in data:
-				counts = convo["countsMonthly"][date]
-				if separate:
-					rowData.append(counts["sent"])
-					rowData.append(counts["received"])
-				else:
-					rowData.append(int(counts["sent"] + counts["received"]))
-
-			rowData.insert(0, date)
-			writer.writerow(rowData)
-	return
-
-# get message data and counts from JSON
-rawData = readJSON()
-
-# parse the data into a more usable format
-data = parseData(rawData, 20, printTotals=True, characters=True)
-
-# write data to a .csv file for excel
-fileName = "output.csv"
-writeData(data, fileName, separate=False)
-
-#done!
-print("Done! Saved to \"{}\".".format(fileName))
+month_sums.to_csv('messagedata.csv')
