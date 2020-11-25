1668c2b9f9623aa3107d1b38cd1229a1ba3a122b
Brennan McMicking <brennanmcmicking@gmail.com>
2020-11-25 11:35:42 -0800

    Reverting to Colin's code

--- messageAnalysis.py
+++ messageAnalysis.py
@@ -1,10 +1,8 @@
 
 # Standard library imports
-import os
 import glob
 import json
 import argparse
-from numpy.lib.shape_base import tile
 
 # Third-party imports
 import pandas as pd
@@ -15,127 +13,85 @@ parser = argparse.ArgumentParser(description='''
     be in the current working directory. 
 ''')
 parser.add_argument(
-    '--data',
-    help='Location of the messages.json file as provided by Instagram',
-    required=True
-)
-
-parser.add_argument(
-    '--name',
-    help='Name of the owner facebook, as it appear in messages download.',
+    "--name",
+    help="Name of the owner facebook, as it appear in messages download.",
     required=True
 )
 parser.add_argument(
-    '--friends',
-    nargs='+',
-    help='Name of friends to include in stats.',
+    "--friends",
+    nargs="+",
+    help="Name of friends to include in stats.",
     required=True
-)
-parser.add_argument(
-    '--outdir',
-    help='Default output directory for plots',
-    default='output/'
 )
 args = parser.parse_args()
 filePath = args.data
 my_name = args.name
 friends = args.friends
-outdir = args.outdir
-
-# print(f'Friends: {friends}')
-
-# create the output directory if it does not already exist
-if not os.path.exists(outdir):
-    os.makedirs(outdir)
 
 # iterate over every message file, grabbing data from each
 rows = []
+for filePath in glob.glob("messages/inbox/**/message_*.json"):
 
-# read the message file into a dictionary
-message_file_json = ''
-with open(filePath, encoding='UTF-8') as file:
-    message_file_json = file.read()
-message_file = json.loads(message_file_json)
+    # read the message file into a dictionary
+    message_file_json = ""
+    with open(filePath) as file:
+        message_file_json = file.read()
+    message_file = json.loads(message_file_json)
 
 for thread in message_file:
     # get message file metadata
-    participants = thread['participants']
-    messages = thread['conversation']
+    participants = message_file["participants"]
+    messages = message_file["messages"]
 
-    # print(f'Number of participants: {len(participants)}')
+    # discard group chats
+    if len(participants) != 2:
+        continue
+
+    # get the name of the friend the current message file is for
+    friend_name = participants[0]["name"]
 
     # create a row for each message
     for message in messages:
 
-        # note: we are also going to include images as a message
-        # sending memes to your friends counts.
+        # ignore non-text messages e.g. pictures, shares, calls etc.
+        msgtype = message.get('type')
+        if msgtype != 'Generic':
+            continue
 
-        # we want to include group chat data in our design, so if we come across a message that our user sent, we have to add a data point for every
-        # person that saw it
-        # later we coulc change it to showing a line for group chats but we the data doesnt include names and it also stores the same group
-        # in multiple threads which makes it difficult to tell if we've already seen this group chat
-        if message.get('sender') == my_name:
-            # print(f'found sent message to: ')
-            for person in participants:
-                # print(f'\t{person}')
-                if person != my_name:
-                    content = message.get('text')
-                    rows.append({
-                        'friend': person,
-                        'received': False,
-                        'length': len(content) if content else 0,
-                        'timestamp': message.get('created_at')
-                    })
-        else:
-            content = message.get('text')
-            rows.append({
-                'friend': message.get('sender'),
-                'received': True,
-                'length': len(content) if content else 0,
-                'timestamp': message.get('created_at')
-            })
+        sent_by_friend = message.get("sender_name") != my_name
+        timestamp = message.get("timestamp_ms")
+        content = message.get("content")
+        length = len(content) if content else 0
 
-# load the message data into a pandas dataframe
+        rows.append({
+            'friend': friend_name,
+            'sent_by_friend': sent_by_friend,
+            'length': length,
+            'timestamp': timestamp
+        })
+
+
 df = pd.DataFrame(rows)
-df['timestamp'] = pd.to_datetime(df.timestamp)
+df['timestamp'] = pd.to_datetime(df['timestamp'], unit="ms")
 df['friend'] = pd.Categorical(df.friend)
 
-# save overall stats for all friends
-overall = df.groupby(['friend', 'received']).sum().fillna(0)
-overall.to_csv(f'{outdir}all_friends_total.csv')
-with open(f'{outdir}all_friends_total.txt', 'w') as fp:
-    fp.write(overall.to_string())
+for x in df['friend'].unique():
+    print(x)
+df = df[df['friend'].isin(friends)]
 
-if 'all' not in friends:
-    # do further analysis with only the specified friends
-    df = df[df['friend'].isin(friends)]
-df = df.set_index('timestamp')
-month_sums = df.groupby([pd.Grouper(freq='M'), 'friend']).count().dropna()
-del month_sums['received']
+date_index = df.set_index('timestamp')
+month_sums = date_index.groupby(
+    [pd.Grouper(freq="M"), 'friend']).count().dropna()
+del month_sums['sent_by_friend']
 month_sums = month_sums.reset_index().pivot(
     index='timestamp', columns='friend').fillna(0)
-month_sums = month_sums['length']
 
-# create stacked area plot
-area_plot = month_sums.plot.area(
-    figsize=(10, 5), linewidth=0, title='Monthly message counts')
-area_plot.legend(title='Friend Name')
-area_plot.set_xlabel("x label")
-area_plot.set_ylabel("y label")
-area_plot.get_figure().savefig(f'{outdir}stacked.png', dpi=300)
+print(month_sums.to_string())
 
-# create line plot
-line_plot = month_sums.plot.line(lw=1, figsize=(
-    10, 5),  title='Monthly message counts')
-line_plot.legend(title='Friend Name')
-line_plot.set_xlabel("Message Count")
-line_plot.set_ylabel("y label")
-line_plot.get_figure().savefig(f'{outdir}lines.png', dpi=300)
+ax = month_sums.plot.area(figsize=(10, 5), linewidth=0)
+ax.get_figure().savefig('stacked.png', dpi=300)
 
-# create csv file
-month_sums.to_csv(f'{outdir}selected_friends_monthly.csv')
+lines = month_sums.plot.line(figsize=(10, 5))
+lines.get_figure().savefig('lines.png', dpi=300)
 
-
-def print_friend_list(df):
-    for x in df['friend'].unique():
-        print(x)
+month_sums.to_csv('messagedata.csv')
